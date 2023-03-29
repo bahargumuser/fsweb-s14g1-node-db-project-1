958a108b3a758baa09dadfdb4f909e3db4a890b5
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-03-29 14:57:40 +0300

    Completed

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,35 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // KODLAR BURAYA
-}
+  return db("accounts");
+};
 
-const getById = id => {
-  // KODLAR BURAYA
-}
+const getById = (id) => {
+  return db("accounts").where({ id }).first();
+};
 
-const create = account => {
-  // KODLAR BURAYA
-}
+const create = (account) => {
+  const insertedAccount = db("accounts")
+    .insert(account)
+    .then((ids) => {
+      return getById(ids[0]);
+    });
+  return insertedAccount;
+};
 
 const updateById = (id, account) => {
-  // KODLAR BURAYA
-}
+  return db("accounts")
+    .where({ id })
+    .update(account)
+    .then((rows) => {
+      return getById(id);
+    });
+  //satır sayısı return ediliyor
+};
 
-const deleteById = id => {
-  // KODLAR BURAYA
-}
+const deleteById = (id) => {
+  db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +37,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
